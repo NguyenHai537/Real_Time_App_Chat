eb99a6247f8e008dc55345e544969180ef07383e
TUNGHUYNH <you@example.comhuynhthanhtunga1@gmail.com git config --global user.name Your>
2023-03-17 11:01:50 +0700

    tung commit 11h

--- soket-app-chat-client/src/App.js
+++ soket-app-chat-client/src/App.js
@@ -7,17 +7,21 @@ import Chat11 from './components/Chat1-1';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import UserDetails from './components/UserDetails';
+import UpdateInfo from './components/UpdateInfo';
+import Account from './components/Account';
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route  path="/" element={<Login />} />
-      <Route  path="/chat/:username" element={<Chat />} />
+      <Route  path="/chat/:id" element={<Chat />} />
       <Route  path="/chat1-1/:username/:value" element={<Chat11 />} />
+      <Route path="/:id/updateinfo" element={<UpdateInfo/>}/>
+      <Route path='/:id/account' element={<Account/>}/>
     </Routes>
     </BrowserRouter>
   );
 }
-
+// Tung them 2 router xu ly userInformation 10h 17/3
 export default App;
