e9671b120bd75b9766dc9f07a4dce76906fc79b2
TUNGHUYNH <you@example.comhuynhthanhtunga1@gmail.com git config --global user.name Your>
2023-03-17 16:17:56 +0700

    tung 4h17

--- soket-app-chat-client/src/components/UserDetails.js
+++ soket-app-chat-client/src/components/UserDetails.js
@@ -1,43 +1,69 @@
-// import { useParams } from "react-router-dom"
+import React from 'react'
+import '../UpdateInfo.css'
+function UserDetails() {
+    return (
+        <body>
+        <div class="container">
+    <div class="row">
+      <div class="col-lg-10 col-xl-9 mx-auto">
+        <div class="card flex-row my-5 border-0 shadow rounded-3 overflow-hidden">
+          <div class="card-img-left d-none d-md-flex">
 
-// export default function UserDetails() {
-//       const [userList, setUserList] = useState([]);
-    
-//       useEffect(() => {
-//         axios
-//           .get(`http://localhost:8080/get-user-by-id`)
-//           .then(res => {
-//             setUserList(res.data);
-//           })
-//           .catch(err => {
-//             throw err;
-//           });
-//       }, []);
-    
-//       return (
-//         <div>
-//           <table>
-//             <thead>
-//               <tr>
-//                 <th>Id</th>
-//                 <th>Name</th>
-//                 <th>Email</th>
-//                 <th>Password</th>
-//                 <th>Image</th>
-//               </tr>
-//             </thead>
-//             <tbody>
-//               {userList.map(user => (
-//                 <tr key={user.id}>
-//                   <td>{user.id}</td>
-//                   <td>{user.name}</td>
-//                   <td>{user.email}</td>
-//                   <td>{user.password}</td>
-//                   <td><img src={user.image} alt="img"/></td>
-//                 </tr>
-//               ))}
-//             </tbody>
-//           </table>
-//         </div>
-//       )
-//     }
+          </div>
+          <div class="card-body p-4 p-sm-5">
+            <h5 class="card-title text-center mb-5 fw-light fs-5">Register</h5>
+            <form>
+
+              <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingInputUsername" placeholder="myusername" required autofocus/>
+                <label for="floatingInputUsername">Username</label>
+              </div>
+
+              <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInputEmail" placeholder="name@example.com"/>
+                <label for="floatingInputEmail">Email address</label>
+              </div>
+
+              <hr/>
+
+              <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
+                <label for="floatingPassword">Password</label>
+              </div>
+
+              <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="floatingPasswordConfirm" placeholder="Confirm Password"/>
+                <label for="floatingPasswordConfirm">Confirm Password</label>
+              </div>
+
+              <div class="d-grid mb-2">
+                <button class="btn btn-lg btn-primary btn-login fw-bold text-uppercase" type="submit">Register</button>
+              </div>
+
+              <a class="d-block text-center mt-2 small" href="#">Have an account? Sign In</a>
+
+              <hr class="my-4"/>
+
+              <div class="d-grid mb-2">
+                <button class="btn btn-lg btn-google btn-login fw-bold text-uppercase" type="submit">
+                  <i class="fab fa-google me-2"></i> Sign up with Google
+                </button>
+              </div>
+
+              <div class="d-grid">
+                <button class="btn btn-lg btn-facebook btn-login fw-bold text-uppercase" type="submit">
+                  <i class="fab fa-facebook-f me-2"></i> Sign up with Facebook
+                </button>
+              </div>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </body>
+    )
+}
+
+export default UserDetails
